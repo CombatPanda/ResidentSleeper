09944468b5e2e589db0097bf1131bd6204aca522
Šaltenis Bartas <bartas.saltenis@mif.stud.vu.lt>
2022-04-21 19:51:54 +0300

    Created controllers/models/db

---
+++ ResidentSleeper/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResidentSleeper.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public string userID { get; set; }
+        public int orderDetailID { get; set; }
+        public int status { get; set; }
+        public double cost { get; set; }
+    }
+}
