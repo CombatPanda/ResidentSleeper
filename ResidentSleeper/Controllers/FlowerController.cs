a808be3db55a139810a0b7224defb8d275a2bd45
ddovile <71456312+ddovile@users.noreply.github.com>
2022-04-26 21:13:50 +0300

    Service and controller for flowers

--- ResidentSleeper/Controllers/FlowerController.cs
+++ ResidentSleeper/Controllers/FlowerController.cs
@@ -1,11 +1,69 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using ResidentSleeper.Models;
+using ResidentSleeper.Service.FlowerService;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ResidentSleeper.Controllers
 {
-    public class FlowerController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlowerController : ControllerBase
     {
+        private readonly IFlowerService _service;
+
+        public FlowerController(IFlowerService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/Flower
+        [HttpGet]
+        public async Task<List<Flower>> GetAll() 
+        {
+            return await _service.GetAll(); 
+        }
+
+        // GET: api/Flower/id/10
+        [HttpGet("id/{id}")]
+        public async Task<Flower> GetById(int id)
+        {
+            return await _service.GetById(id);
+        }
+
+        // GET: api/Flower/type/5
+        [HttpGet("type/{typeId}")]
+        public async Task<List<Flower>> GetByType(int typeId)
+        {
+            return await _service.GetByType(typeId);
+        }
+
+        // GET: api/Flower/name/tulpe
+        [HttpGet("name/{typeId}")]
+        public async Task<List<Flower>> GetByName(string name)
+        {
+            return await _service.GetByName(name);
+        }
+
+        // POST: api/Flower
+        [HttpPost]
+        public async Task Add (Flower flower)
+        {
+            await _service.Add(flower);
+        }
+
+        // PUT: api/Flower/10
+        [HttpPut("{id}")]
+        public async Task Edit(int id, Flower flower)
+        {
+            await _service.Edit(id, flower);
+        }
+
+        // DELETE: api/Flower/10
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _service.Delete(id);
+        }
     }
 }
---
+++ ResidentSleeper/Service/FlowerService/IFlowerService.cs
@@ -0,0 +1,18 @@
+﻿using ResidentSleeper.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ResidentSleeper.Service.FlowerService
+{
+    public interface IFlowerService
+    {
+        Task<Flower> GetById(int id);
+        Task<List<Flower>> GetAll();
+        Task<List<Flower>> GetByName(string name);
+        Task<List<Flower>> GetByType(int typeId);
+        Task Add(Flower flower);
+        Task Edit(int id, Flower flower);
+        Task Delete(int id);
+        Task SellFlower(int id, int amount);
+    }
+}
