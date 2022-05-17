ee0f71e35770f03d18d550bdaae61b2220e4fbac
Ruimtevaart <granata0520@gmail.com>
2022-05-17 18:04:59 +0300

    Added pagination component

--- ResidentSleeper/ClientApp/src/components/Flowers.js
+++ ResidentSleeper/ClientApp/src/components/Flowers.js
@@ -1,6 +1,6 @@
-﻿import React, { Component } from 'react';
+﻿import React, { Component, useState } from 'react';
 import FlowerCard from './FlowerCard';
-import { Grid } from '@mui/material';
+import { Grid, Pagination, Box} from '@mui/material';
 import FlowerCardSkeleton from './FlowerCardSkeleton';
 
 export class Flowers extends Component {
@@ -10,9 +10,11 @@ export class Flowers extends Component {
         super(props);
         this.state = {
             flowers: [],
-            loading: true
+            loading: true,
+            page: 1
         };
         this.addToCart = this.addToCart.bind(this);
+        
     }
 
     addToCart(id) {
@@ -35,7 +37,8 @@ export class Flowers extends Component {
     static renderFlowersTable(flowers) {
         return (
             <Grid container spacing={2}>
-                {flowers.map(flower => (
+                {/*{flowers.slice((this.state.page - 1) * 9, this.state.page * 9).map(flower => (*/}
+                {flowers.slice(0, 9).map(flower => (
                     <Grid item xs={4}>
                         <FlowerCard imUrl={flower.pictureURL} title={flower.name} description={flower.description} id={flower.id} cost={flower.cost} />
                     </Grid>
@@ -61,6 +64,9 @@ export class Flowers extends Component {
             <div>
                 <h1 id="tabelLabel" >Flower list</h1>
                 {contents}
+                <Box alignItems="center" justifyContent="center" display="flex" mt={2} mb={2}>
+                    <Pagination count={Math.ceil(this.state.flowers.length / 9)} page={this.state.page} defaultPage={1} onChange={(event, value) => this.setState({ page: value })} />
+                </Box>
             </div>
         );
     }
