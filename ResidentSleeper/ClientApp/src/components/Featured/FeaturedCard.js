be77d5fcdf3aad2022db93922e351d291c45492c
Ruimtevaart <granata0520@gmail.com>
2022-05-31 02:20:36 +0300

    Added featured items to home, animations

---
+++ ResidentSleeper/ClientApp/src/components/Featured/FeaturedCard.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import { Card, Fade, Typography } from '@mui/material';
+import { Grid } from '@material-ui/core';
+
+export default function FeaturedCard(props) {
+  return (
+    <Fade in={props.fadeIn}>
+      <Card>
+        <Grid height="500px" style={{ backgroundImage: `url(${props.imUrl})`, backgroundSize: "cover", backgroundPosition: "center" }} container direction="column" alignItems="center" justifyContent="center">
+          <Grid item>
+            <Typography align="center" gutterBottom variant="h4" component="div" color="white">{props.title}</Typography>
+            <Typography align="center" gutterBottom variant="h4" component="div" color="white">{props.cost}€</Typography>
+          </Grid>
+        </Grid>
+      </Card>
+    </Fade>
+  );
+}
--- ResidentSleeper/ClientApp/src/components/Home.js
+++ ResidentSleeper/ClientApp/src/components/Home.js
@@ -1,25 +1,68 @@
-import { Button, Grid, Paper, Typography } from '@mui/material';
-import { lightBlue } from '@mui/material/colors';
+import { Grid, Typography } from '@mui/material';
 import React, { Component } from 'react';
-import CarouselCard from './Carousel/CarouselCard';
+import FeaturedCard from './Featured/FeaturedCard';
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flowers: [],
+      loading: true,
+      index: 0,
+      fade: true
+    };
+  }
+
+  componentDidMount() {
+    this.populateFlower();
+  }
+
   static displayName = Home.name;
 
-  render () {
+  static renderCarousel(flowers, index, fade) {
+    return (
+      <Grid container spacing={{ xs: 2, md: 3 }} sx={{ pt: 2 }}>
+        <Grid item xs={12}><Typography variant="h5">This month's featured items:</Typography></Grid>
+        <Grid item xs={4}>
+          <FeaturedCard imUrl={flowers[index].pictureURL} title={flowers[index].name} cost={flowers[index].cost} fadeIn={fade} />
+        </Grid>
+        <Grid item xs={4}>
+          <FeaturedCard imUrl={flowers[index + 1].pictureURL} title={flowers[index + 1].name} cost={flowers[index + 1].cost} fadeIn={fade} />
+        </Grid>
+        <Grid item xs={4}>
+          <FeaturedCard imUrl={flowers[index + 2].pictureURL} title={flowers[index + 2].name} cost={flowers[index + 2].cost} fadeIn={fade} />
+        </Grid>
+      </Grid>
+    );
+  }
+
+  render() {
+    let contents = this.state.loading
+      ? ""
+      : Home.renderCarousel(this.state.flowers, this.state.index, this.state.fade);
     return (
       <div>
         <Grid container style={{ backgroundImage: `url(${"https://travelbristol.org/wp-content/uploads/2022/02/anthony-fomin-h7_SyoBhHF0-unsplash-scaled.jpg"})`, height: "400px", backgroundSize: "cover" }} direction="row" justifyContent="center" alignItems="center">
-            <Grid item md={12} container justifyContent="center" direction="column" alignItems="center">
-                <Typography variant="h2" color="white" gutterBottom>
-                    ResidentSleeper flower shop
-                </Typography>
-                <Typography variant="body1" color="white" paragraph>
-                    Only the freshest and most beautiful flowers you can find
-                </Typography>
-            </Grid>
+          <Grid item md={12} container justifyContent="center" direction="column" alignItems="center">
+            <Typography variant="h2" color="white" gutterBottom>ResidentSleeper flower shop</Typography>
+            <Typography variant="body1" color="white" paragraph>Only the freshest and most beautiful flowers you can find</Typography>
+          </Grid>
         </Grid>
+        {contents}
       </div>
     );
+  }
+
+  async populateFlower() {
+    const response = await fetch('api/Flower');
+    const data = await response.json();
+    console.log(data);
+    this.setState({ flowers: data, loading: false, index: Math.floor(Math.random() * data.length) });
+    setInterval(() => {
+      this.setState({ fade: false });
+      setTimeout(() => { this.setState({ index: Math.floor(Math.random() * (data.length - 2)) }) }, 100);
+      console.log("New index: " + this.state.index);
+      setTimeout(() => { this.setState({ fade: true }) }, 500);
+    }, 10000);
   }
 }
